9fe61906336aaff3f3fd6f53209885a7496682b2
hirossan4049 <haruto405329@gmail.com>
2025-09-12 01:21:23 +0900

    chore: ui fix

--- src/components/admin/discord-management.tsx
+++ src/components/admin/discord-management.tsx
@@ -264,7 +264,7 @@ function DiscordMembersDialog({
 
   return (
     <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent className="max-w-4xl">
+      <DialogContent className="max-w-4xl max-h-[85vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>{t("membersList")} - {guildId}</DialogTitle>
         </DialogHeader>
